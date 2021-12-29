a796510655371287653d5f5e78a6808ba05d8e88
MustafaPaigeer <paigeer2012@gmail.com>
2021-12-28 23:00:01 -0800

    School age finder

---
+++ schoolagefunc.js
@@ -0,0 +1,21 @@
+const whichSchool  = function (age) {
+    // Your code in here ...    
+    if (age<13){
+        console.log("You shoud go to elemantry school")
+    }else if(age>=13 && age<=18){
+        console.log("You should go to secondry school")
+    }else
+        console.log("Lighthouse Labs")
+    }
+
+    for (i = 19; i < 100; i++) {
+        expect(whichSchool(i)).to.be.equal("Lighthouse Labs");
+        console.log(i)
+      }
+
+//   console.log("I am 18. Which school should I go to?");
+// console.log(whichSchool(18));
+// console.log("I am 8. Which school should I go to?");
+// console.log(whichSchool(8));
+// console.log("I am 19. Which school should I go to?");
+// console.log(whichSchool(19));
